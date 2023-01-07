9092f9634420fac02a50f6b8e78303f2846faa6e
Jordan Barrett <jordan.barrett@canonical.com>
2023-01-08 10:39:45 +1300

    [Controller] fill in gameplay logic - fix passing logic - add HandResult

--- controller.go
+++ controller.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	c "github.com/barrettj12/collections"
 )
 
@@ -14,6 +16,10 @@ type Controller struct {
 	kitty      *c.List[Card]
 	bid        Bid
 	contractor int
+
+	leader    int
+	tricks    [10]*c.List[Card]
+	tricksWon [4]int
 }
 
 // Play plays this game of 500.
@@ -41,7 +47,16 @@ func (ct *Controller) Play() {
 			bidder = (bidder + 1) % 4
 			continue
 		}
-		if hasPassed.Count(func(i int, b bool) bool { return b == true }) == 3 {
+
+		numPasses := hasPassed.Count(func(i int, b bool) bool { return b == true })
+		if numPasses == 4 {
+			// All players passed - re-deal
+			for i := 0; i < 4; i++ {
+				ct.players[i].NotifyHandResult(redeal{})
+			}
+			return
+		}
+		if winningBid != nil && numPasses == 3 {
 			// All other players have passed - bid is won
 			break
 		}
@@ -70,10 +85,6 @@ func (ct *Controller) Play() {
 		bidder = (bidder + 1) % 4
 	}
 
-	if winningBidder == -1 {
-		// Everyone passed - re-deal
-		return
-	}
 	ct.bid = winningBid
 	ct.contractor = winningBidder
 
@@ -96,12 +107,55 @@ func (ct *Controller) Play() {
 	})
 	ct.hands[ct.contractor] = ct.hands[ct.contractor].Filter(func(i int, _ Card) bool { return !toDrop.Contains(i) })
 
-	// - ask player for play
-	// - check play is valid
-	// - notify player if play is valid or not
-	// - notify all players of play
-	// - ...
-	// - notify players of trick winner
+	// Play game
+	ct.leader = ct.contractor
+	for trickNum := 0; trickNum < 10; trickNum++ {
+		ct.tricks[trickNum] = c.NewList[Card](4)
+
+		for i := 0; i < 4; i++ {
+			playerNum := (i + ct.leader) % 4
+
+			validPlays := ct.bid.ValidPlays(ct.tricks[trickNum], ct.hands[playerNum])
+			cardNum := retryTillValid(func() (int, bool) {
+				cardNum := ct.players[playerNum].Play(
+					ct.tricks[trickNum], // trick so far
+					validPlays,
+				)
+				return cardNum, validPlays.Contains(cardNum)
+			})
+
+			card := E(ct.hands[playerNum].Remove(cardNum))
+			ct.tricks[trickNum].Append(card)
+
+			// Notify players of played card
+			for i := 0; i < 4; i++ {
+				ct.players[i].NotifyPlay(playerNum, card)
+			}
+			ct.players[playerNum].NotifyHand(ct.hands[playerNum])
+		}
+
+		// Determine winner
+		winner := ct.whoWins(trickNum)
+		ct.tricksWon[winner]++
+		ct.leader = winner
+
+		for i := 0; i < 4; i++ {
+			ct.players[i].NotifyTrickWinner(winner)
+		}
+	}
+
+	// Determine hand result
+	teamTricks := ct.tricksWon[ct.contractor] + ct.tricksWon[(ct.contractor+2)%4]
+	var res HandResult
+	if ct.bid.Won(teamTricks) {
+		res = bidWon{ct.bid, teamTricks}
+	} else {
+		res = bidWon{ct.bid, teamTricks}
+	}
+
+	for i := 0; i < 4; i++ {
+		ct.players[i].NotifyHandResult(res)
+	}
 }
 
 // retryTillValid repeatedly calls the given function until it returns a true
@@ -115,6 +169,25 @@ func retryTillValid[T any](f func() (T, bool)) T {
 	}
 }
 
+func (ct *Controller) whoWins(trickNum int) int {
+	leadCard := E(ct.tricks[trickNum].Get(0))
+	order := ct.bid.CardOrder(leadCard)
+	winner := ct.leader // whoever lead wins by default
+
+	for _, card := range *order {
+		i, err := ct.tricks[trickNum].Find(card)
+		if err != nil {
+			// not found
+			continue
+		}
+
+		winner = i
+		break
+	}
+
+	return winner
+}
+
 // Player represents a 500 player. The controller interacts with the player
 // in two distinct ways:
 //   - Events: the controller informs a player of something that has happened
@@ -125,8 +198,48 @@ type Player interface {
 	// Events
 	NotifyHand(*c.List[Card])
 	NotifyBid(player int, bid Bid)
+	NotifyPlay(player int, card Card)
+	NotifyTrickWinner(player int)
+	NotifyHandResult(res HandResult)
 
 	// Requests
 	Bid() Bid
 	Drop3() *c.Set[int]
+	// Play asks the player to play a card on the given trick.
+	// The returned response must be an element of validPlays.
+	Play(trick *c.List[Card], validPlays *c.List[int]) int
+}
+
+// HandResult represents the outcome of a hand.
+type HandResult interface {
+	Info() string
+}
+
+// redeal is a HandResult representing all players passing during bidding.
+type redeal struct{}
+
+func (r redeal) Info() string {
+	return "Re-deal due to all players passing"
+}
+
+// bidWon says that the contractor won their bid.
+type bidWon struct {
+	bid    Bid
+	tricks int
+}
+
+func (r bidWon) Info() string {
+	return fmt.Sprintf("Contractors won their bid of %s with %d tricks",
+		r.bid, r.tricks)
+}
+
+// bidLost says that the contractors lost their bid.
+type bidLost struct {
+	bid    Bid
+	tricks int
+}
+
+func (r bidLost) Info() string {
+	return fmt.Sprintf("Contractors lost their bid of %s with %d tricks",
+		r.bid, r.tricks)
 }
